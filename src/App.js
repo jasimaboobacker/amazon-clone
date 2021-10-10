05f183a68228a6bee067ff3489145f8fe5b7ebab
aboobackerj <jasim.aboobacker@kpn.com>
2021-10-10 11:33:59 +0200

    Home and product page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     //BEM
     <div className="App">
-      {/* <h1>Hello</h1> */}
       <Header></Header>
-      {/* Home */}
+      <Home></Home>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/61ASx7NHTWL._SX3740_.jpg"
+          alt=""
+        />
+        <div className="home__row">
+          <Product
+            title="Win (Windsor Horne Lockwood III Book 1)"
+            price="14.55"
+            image="https://images-na.ssl-images-amazon.com/images/I/415TAth-PAL._AC_SX184_.jpg"
+            rating={4}
+          ></Product>
+          <Product></Product>
+        </div>
+        <div className="home__row">
+          <Product></Product>
+          <Product></Product>
+          <Product></Product>
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="home__row">
+          <Product></Product>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
